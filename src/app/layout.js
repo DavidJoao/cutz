a20a4e2507231d0c12050965dd5ffa71b8624c9d
DavidJoao <davidsandoval596@gmail.com>
2024-04-09 19:32:32 -0700

    ADD Provider to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Provider from "./components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Cutz: Haircut Appointment",
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        <Provider>
+          {children}
+        </Provider>
+      </body>
     </html>
   );
 }
