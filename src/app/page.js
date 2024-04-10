0a95288a3e8dca6c16caee56dba1f74bd89c19b0
DavidJoao <davidsandoval596@gmail.com>
2024-04-09 19:21:45 -0700

    CREATE SessionProvider and removed unnecessary code

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,7 @@ import axios from 'axios'
 export default function Home() {
   return (
     <main>
-      <button onClick={async (e) => {
-        e.preventDefault()
-        await axios.post('/api/signup')
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
-      }}>Push</button>
+      
     </main>
   );
 }
