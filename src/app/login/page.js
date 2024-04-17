db722d211ebb8c629c05ae8afbd0cdf00f41f5c4
DavidJoao <davidsandoval596@gmail.com>
2024-04-16 21:12:55 -0700

    CREATE Login Page and initial login form
    Need to create backend route to make axios call and create validation

---
+++ src/app/login/page.js
@@ -0,0 +1,49 @@
+'use client'
+import React, { useState } from 'react'
+import { useForm, getValues } from 'react-hook-form'
+
+const Page = () => {
+
+    const passwordCheck = {
+        password: "",
+        confirmPassword: ""
+    }
+
+    const [passwordObj, setPasswordObj] = useState(passwordCheck)
+
+    const { 
+        register,
+        handleSubmit,
+        watch,
+        getValues,
+        formState: { errors },
+    } = useForm();
+
+    const handleChange = (e) => {
+        const { value, name } = e.target
+        setPasswordObj({
+            ...passwordObj,
+            [name] : value
+        })
+    }
+
+    const onSubmit = (data) => console.log(data)
+
+  return (
+    <div className='w-full h-screen bg-black flex flex-col items-center justify-center'>
+        <form className="flex flex-col gap-3 text-white" onSubmit={handleSubmit(onSubmit)}>
+            <input className='input w-[340px]' required placeholder="First Name" {...register("firstName")}/>
+            <input className='input w-[340px]' required placeholder="Second Name" {...register("secondName")}/>
+            <label>Date of Birth:</label>
+            <input className='input w-[340px]' required placeholder="Date of Birth:" type="date" {...register("dob")}/>
+            <input className='input w-[340px]' required placeholder="Email" {...register("email")}/>
+            <input className='input w-[340px]' required name="password" type="password" placeholder="Password" {...register("password")} onChange={handleChange}/>
+            <input className='input w-[340px]' required name="confirmPassword" type="password" placeholder="Confirm Password" {...register("confirmPassword")} onChange={handleChange}/>
+            <input className='input w-[340px]' required type="number" placeholder="Phone" {...register("phone")}/>
+            { passwordObj.password === passwordObj.confirmPassword ? ( <input type='submit' className='primary-button' /> ) : ( <></> )}
+        </form>
+    </div>
+  )
+}
+
+export default Page
