41c913a865151b1ea1ef55ae1f9807f00a042d69
DavidJoao <davidsandoval596@gmail.com>
2024-04-16 20:03:35 -0700

    ADD Navigation links for login and signup

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import axios from 'axios'
 import {colors} from "./custom/colors";
+import Link from 'next/link'
 
 export default function Home() {
 
@@ -15,7 +16,7 @@ export default function Home() {
 
   return (
     <main className="border-[1px] border-black h-screen w-screen flex flex-col items-center p-3 md:p-10 bg-black">
-      <div className={`border-[1px] border-black w-[100%] md:w-[50%] h-[95%] md:h-auto p-3 bg-[#262626]`}>
+      <div className={`border-[1px] border-black w-[100%] md:w-[50%] h-[95%] md:h-auto p-3 text-white rounded`}>
         {/* WELCOME PAGE - DIRECT TO LOGIN PAGE | IGNORE CODE BELOW FOR NOW */}
 
 
@@ -29,6 +30,13 @@ export default function Home() {
           <input type="password" placeholder="Confirm Password"/>
           <input name="phone" type="number" placeholder="Phone"/>
         </form> */}
+        <h1 className="text-center font-bold text-5xl">Cutz</h1>
+        <div className="flex flex-col items-center gap-5">
+          <h2 className="text-xl">Ready for your next appointment?</h2>
+          <Link href={'/login'} className="primary-button">Log In</Link>
+          <p>Or</p>
+          <Link href={'/signup'} className="primary-button">Sign Up</Link>
+        </div>
       </div>
     </main>
   );
