6c95e04439d0d3415e98ef2d1d3289f3d4d10b5b
DavidJoao <davidsandoval596@gmail.com>
2024-04-16 22:09:57 -0700

    CHANGE Signup page from login to signup

--- src/app/page.js
+++ src/app/page.js
@@ -17,19 +17,6 @@ export default function Home() {
   return (
     <main className="border-[1px] border-black h-screen w-screen flex flex-col items-center p-3 md:p-10 bg-black">
       <div className={`border-[1px] border-black w-[100%] md:w-[50%] h-[95%] md:h-auto p-3 text-white rounded`}>
-        {/* WELCOME PAGE - DIRECT TO LOGIN PAGE | IGNORE CODE BELOW FOR NOW */}
-
-
-        {/* <form className="flex flex-col gap-3 text-white">
-          <input name="firstName" placeholder="First Name" />
-          <input name="secondName" placeholder="Second Name"/>
-          <label>Date of Birth:</label>
-          <input name="dob" placeholder="Date of Birth:" type="date" className="text-black"/>
-          <input name="email" placeholder="Email"/>
-          <input name="password" type="password" placeholder="Password"/>
-          <input type="password" placeholder="Confirm Password"/>
-          <input name="phone" type="number" placeholder="Phone"/>
-        </form> */}
         <h1 className="text-center font-bold text-5xl">Cutz</h1>
         <div className="flex flex-col items-center gap-5">
           <h2 className="text-xl">Ready for your next appointment?</h2>
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -31,6 +31,7 @@ const Page = () => {
 
   return (
     <div className='w-full h-screen bg-black flex flex-col items-center justify-center'>
+        <h1 className="text-center font-bold text-5xl text-white mb-5">Cutz</h1>
         <form className="flex flex-col gap-3 text-white" onSubmit={handleSubmit(onSubmit)}>
             <input className='input w-[340px]' required placeholder="First Name" {...register("firstName")}/>
             <input className='input w-[340px]' required placeholder="Second Name" {...register("secondName")}/>
@@ -40,7 +41,7 @@ const Page = () => {
             <input className='input w-[340px]' required name="password" type="password" placeholder="Password" {...register("password")} onChange={handleChange}/>
             <input className='input w-[340px]' required name="confirmPassword" type="password" placeholder="Confirm Password" {...register("confirmPassword")} onChange={handleChange}/>
             <input className='input w-[340px]' required type="number" placeholder="Phone" {...register("phone")}/>
-            { passwordObj.password === passwordObj.confirmPassword ? ( <input type='submit' className='primary-button' /> ) : ( <></> )}
+            { passwordObj.password === passwordObj.confirmPassword ? ( <input value={"Register"} type='submit' className='primary-button' /> ) : ( <></> )}
         </form>
     </div>
   )
