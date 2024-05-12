14ed7a94559ecb9757e089811d6a6bff64c1225a
DavidJoao <davidsandoval596@gmail.com>
2024-05-11 21:37:06 -0700

    CHANGE Redirection link and title to page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -17,13 +17,13 @@ const Login = () => {
 
   return (
     <div className='w-full h-screen bg-black flex flex-col items-center justify-center text-white'>
-      <h1 className="text-center font-bold text-5xl">Cutz</h1>
+      <h1 className="text-center font-bold text-5xl">Manage It</h1>
       <form className="flex flex-col gap-5 mt-4" onSubmit={handleSubmit(onSubmit)}>
         <input placeholder='Email' className='input' {...register("email")}/> 
         <input placeholder='********' type='password' className='input' {...register("password")}/>
         <input type='submit' className='primary-button' value={"Log In"}/>
       </form>
-      <Link href={"/signup"} className="text-white mt-3">Don't have an account? Sign Up!</Link>
+      <Link href={"/"} className="text-white mt-3">Don't have an account? Sign Up!</Link>
     </div>
   )
 }
