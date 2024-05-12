4d6d3387c0b29a2c1f87659cebfc8832eaef262c
DavidJoao <davidsandoval596@gmail.com>
2024-05-11 21:37:27 -0700

    CHANGE Routes to new signup pages

--- src/app/page.js
+++ src/app/page.js
@@ -17,12 +17,15 @@ export default function Home() {
   return (
     <main className="border-[1px] border-black h-screen w-screen flex flex-col items-center p-3 md:p-10 bg-black">
       <div className={`border-[1px] border-black w-[100%] md:w-[50%] h-[95%] md:h-auto p-3 text-white rounded`}>
-        <h1 className="text-center font-bold text-5xl">Cutz</h1>
+        <h1 className="text-center font-bold text-5xl">Manage It</h1>
         <div className="flex flex-col items-center gap-5">
-          <h2 className="text-xl">Ready for your next appointment?</h2>
+          <h2 className="text-xl">Ready to make things easier?</h2>
           <Link href={'/login'} className="primary-button">Log In</Link>
           <p>Or</p>
-          <Link href={'/signup'} className="primary-button">Sign Up</Link>
+          <div className="flex gap-2">
+            <Link href={'/employee-signup'} className="primary-button">Sign Up As Employee</Link>
+            <Link href={'/employer-signup'} className="primary-button">Sign Up As Employer</Link>
+          </div>
         </div>
       </div>
     </main>
