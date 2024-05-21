c3a30f0612a9abcbe13f0c0e6f7aa949ad94b426
DavidJoao <davidsandoval596@gmail.com>
2024-05-20 21:45:35 -0700

    REMOVED Unused functions from page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,6 @@ import Link from 'next/link'
 
 export default function Home() {
 
-  const initialUser = {
-
-  }
-
-  const handleChange = () => {
-
-  }
-
   return (
     <main className="border-[1px] border-black h-screen w-screen flex flex-col items-center p-3 md:p-10 bg-black">
       <div className={`border-[1px] border-black w-[100%] md:w-[50%] h-[95%] md:h-auto p-3 text-white rounded`}>
