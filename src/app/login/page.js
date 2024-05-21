c61b4acabb5abc2f40028bf4dad2bc383dafb302
DavidJoao <davidsandoval596@gmail.com>
2024-05-20 21:45:05 -0700

    MODIFIED File to use only client side functions importing server side functions

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,51 +1,38 @@
 'use client'
-import React, { useEffect } from 'react'
-import { useForm, getValues } from 'react-hook-form'
+import React, { useState } from 'react'
 import Link from 'next/link'
-import { signIn } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import { navigate } from '../custom/redirect'
+import { authenticate, logSession } from '../custom/actions'
 
-const Login = async () => {
+const Login = () => {
 
-  const { 
-    register,
-    handleSubmit,
-    watch,
-    reset,
-    getValues,
-    formState: { errors },
-} = useForm();
-
-  const onSubmit = async (data) => {
-
-      const signInData = await signIn('credentials', {
-        email: data.email,
-        password: data.password,
-        redirect: false
-      })
-
-      if (signInData?.ok === false) {
-        console.error(signInData.error)
-        // setMessage('Incorrect password or email')
-      } else {
-        console.log('Logged In Successfully')
-        console.log(signInData)
-        // router.push('/dashboard')
-        // router.refresh();
-      }
-      
+  const initialFormData = {
+    email: '',
+    password: ''
   }
+
+  const [formData, setFormData] = useState(initialFormData)
+
+  const handleChanges = (e) => {
+    const { value, name } = e.target;
+    setFormData({ 
+      ...formData,
+      [name]: value
+    })
+  }
+
 
   return (
     <div className='w-full h-screen bg-black flex flex-col items-center justify-center text-white'>
       <h1 className="text-center font-bold text-5xl">Manage It</h1>
-      <form className="flex flex-col gap-5 mt-4" onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder='Email' className='input' {...register("email")}/> 
-        <input placeholder='********' type='password' className='input' {...register("password")}/>
+      <form className="flex flex-col gap-5 mt-4" action={() => authenticate(formData)}>
+        <input name='email' placeholder='Email' className='input' onChange={handleChanges}/> 
+        <input name='password' placeholder='********' type='password' className='input' onChange={handleChanges}/>
         <input type='submit' className='primary-button' value={"Log In"}/>
       </form>
       <Link href={"/"} className="text-white mt-3">Don't have an account? Sign Up!</Link>
+      <button onClick={() => {
+        logSession().then(res => console.log(res))
+      }}>Click</button>
     </div>
   )
 }
