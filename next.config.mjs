d3c83ae6d2bdfad180e41254763c7f074f754acd
DavidJoao <davidsandoval596@gmail.com>
2024-05-20 21:46:30 -0700

    NextCONFIG

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+    webpack5: true,
+    webpack: (config) => {
+      config.resolve.fallback = { fs: false };
+  
+      return config;
+    },
+  };
---
+++ src/app/api/[...nextauth]/route.js
@@ -0,0 +1,3 @@
+import { handlers } from "@/app/custom/auth";
+
+export const { GET, POST } = handlers;
