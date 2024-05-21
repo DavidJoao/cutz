36628a7394af6f535f264aff9c723e292a9e1f90
DavidJoao <davidsandoval596@gmail.com>
2024-05-20 21:43:21 -0700

    CREATE New nextauth file in app directory

--- pages/api/auth/[...nextauth].js
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth/next";
-import { authOptions } from "@/app/custom/auth";
-
-const handler = NextAuth(authOptions)
-
-export default handler;
-
-export { handler as GET, handler as POST }
