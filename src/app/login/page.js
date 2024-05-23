56b8fb2a15d5f145d1ecfa40063af012e051c883
DavidJoao <davidsandoval596@gmail.com>
2024-05-22 22:16:16 -0700

    FIX Bug after logging in
    After authentication was not loading navbar and needed to refresh or hit double login to redirect to dashboard

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,8 +2,11 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import { authenticate, logSession } from '../custom/actions'
+import { useRouter } from 'next/navigation'
 
 const Login = () => {
+
+  const router = useRouter()
 
   const initialFormData = {
     email: '',
@@ -20,22 +23,18 @@ const Login = () => {
     })
   }
 
-
   return (
     <div className='w-full h-screen bg-black flex flex-col items-center justify-center text-white'>
       <h1 className="text-center font-bold text-5xl">Manage It</h1>
-      <form className="flex flex-col gap-5 mt-4" action={() => authenticate(formData)}>
+      <form className="flex flex-col gap-5 mt-4" action={() => {
+        authenticate(formData)
+        router.refresh()
+        }}>
         <input name='email' placeholder='Email' className='input' onChange={handleChanges}/> 
         <input name='password' placeholder='********' type='password' className='input' onChange={handleChanges}/>
         <input type='submit' className='primary-button' value={"Log In"}/>
       </form>
       <Link href={"/"} className="text-white mt-3">Don't have an account? Sign Up!</Link>
-      <button onClick={() => {
-        logSession().then(res => console.log(res))
-      }}>Click</button>
-      <button onClick={() => {
-        logoutUser().then(res => console.log(res))
-      }}>Click</button>
     </div>
   )
 }
